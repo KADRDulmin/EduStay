313734122e0acc5497517a9408d1ca084262ab5a
RanushVimantha <ranushvimantha1@gmail.com>
2024-04-06 17:39:08 +0530

    Updated Login.Php
    Fixed Names on Login Options

--- login.php
+++ login.php
@@ -43,9 +43,10 @@
             <input type="text" placeholder="Email"/>
             <input type="password" placeholder="password"/>
             <select name="login-as">
-        <option value="admin">Login as Admin</option>
         <option value="student">Login as Student</option>
-        <option value="landowner">Login as Landowner</option>
+        <option value="landowner">Login as Land Lord</option>
+        <option value="landowner">Login as Warden</option>
+        <option value="admin">Login as Admin</option>
     </select>
             <button>login</button>
             <br>
