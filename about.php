3e78d95ca263f78d24136f2b9a298ec7b16fba41
RanushVimantha <ranushvimantha1@gmail.com>
2024-04-08 02:48:51 +0530

    about

--- about.php
+++ about.php
@@ -4,16 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    
     <link rel="stylesheet" href="css/about.css">
 </head>
 <body>
+
 <div class="responsive-container-block bigContainer">
   <div class="responsive-container-block Container">
     <p class="text-blk heading">
       About Us
     </p>
     <p class="text-blk subHeading">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+    EduStay is a revolutionary web application designed to bridge the gap between NSBM Green University students and the array of accommodation options available around the campus.
     </p>
     <div class="social-icons-container">
       <a class="social-icon">
@@ -31,5 +33,6 @@
     </div>
   </div>
 </div>
+<?php include('footer.php'); ?>
 </body>
 </html>
